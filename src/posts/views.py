730ebe0857a12d34be417762f504c74ef2ef2fb0
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 14:54:52 -0400

    Created topic_detail view to fetch single topic and related posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -9,3 +9,14 @@ def topic_list(request):
     }
 
     return render(request, 'posts/topic_list.html', context) # render HTML, passing request and context data
+
+def topic_detail(request, pk):
+    topic = get_object_or_404(Topic, pk=pk) # get Topic by ID or show 404 if not found
+    posts = topic.posts.all().order_by('created_at')  # get all posts for this topic, sorted by oldest first
+
+    context = {
+        'topic': topic, # pass topic object
+        'posts': posts, # and list of related post
+    }
+
+    return render(request, 'posts/topic_detail.html', context) # show the 'topic_detail' page with the given data
