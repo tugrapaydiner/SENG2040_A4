9dbd471957b4f419837e423595e699b1fea79510
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 12:39:25 -0400

    Define Topic model

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL # get current user model
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200) # text, max 200 chars
+    author = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE) # link to user who created (delete topic if user deleted)
+    created_at = models.DateTimeField(auto_now_add=True) # record creation time auto (once)
+    updated_at = models.DateTimeField(auto_now=True) # record but for update (every save)
+    
+def __str__(self):
+    return self.title # display topic's title
