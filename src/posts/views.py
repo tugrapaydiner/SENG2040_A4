56075d61653891295568c396e062d5ade51ea5d0
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 14:01:34 -0400

    Created topic_list view to fetch and prepare topics

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+def topic_list(request):
+    topics = Topic.objects.all().order_by('-created_at') # sort by created_at  (newest first)
+
+    context = {
+        'topics': topics,
+    }
+
+    return render(request, 'posts/topic_list.html', context) # render HTML, passing request and context data
