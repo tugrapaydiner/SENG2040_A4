1406db4e631ba169b03c68bea6065fbdc158c461
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 13:51:29 -0400

    Create and apply initial migrations for posts and profiles apps

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap4',
     'posts.apps.PostsConfig',
     'profiles.apps.ProfilesConfig',
 ]
