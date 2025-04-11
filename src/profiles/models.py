0eeea8890776920845baf66f92e864b2b17b40b5
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 12:49:01 -0400

    Define Profile model structure with OneToOne link to User

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL # get active User model in settings
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # if User deleted, delete Profile
+    # I can add more fields like bio, avatar later
+    created = models.DateTimeField(auto_now_add=True) # record creation time auto (once)
+    updated = models.DateTimeField(auto_now=True) # record update time auto (every save)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}" # show username profile belongs to
