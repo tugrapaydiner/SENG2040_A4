012f86b2eb9c65c7562edef1191717ce39d08163
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 17:50:41 -0400

    Add URL pattern for like_unlike_post view

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views  # import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.topic_list, name='topic_list'),
     path('topic/<int:pk>/', views.topic_detail, name='topic_detail'),
+    path('like/<int:pk>/', views.like_unlike_post, name='like_post'),
 ]
