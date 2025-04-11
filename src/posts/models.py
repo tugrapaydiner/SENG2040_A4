d0f273ff6fe1d4a83f865de4f3e1c2c6dbb56467
Tugrap <106766396+tugrapaydiner@users.noreply.github.com>
2025-04-11 12:45:28 -0400

    Define Post model structure

--- src/posts/models.py
+++ src/posts/models.py
@@ -11,3 +11,19 @@ class Topic(models.Model):
     
 def __str__(self):
     return self.title # display topic's title
+
+class Post(models.Model):
+    content = models.TextField() # field for potentially long text content
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) # link to user who wrote post (delete post if user deleted)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE) # link to topic post belongs to (delete post if topic deleted)
+    created_at = models.DateTimeField(auto_now_add=True) # record creation time auto (once)
+    updated_at = models.DateTimeField(auto_now=True) # record update time auto (every save)
+    liked = models.ManyToManyField(User, related_name='liked_posts', default=None, blank=True) # likes
+
+    def __str__(self):
+        return self.content[:50] + "..." if len(self.content) > 50 else self.content # show first 50 characters (for display)
+
+    # I will add get_photos and like_count methods later
+
+    class Meta:   
+        ordering = ['created_at'] # default ordering when fetched from DB (oldest first)
